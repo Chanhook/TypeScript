a8e0dece43cb78a3377cb89355f20088674a20f0
김찬혁 <cksgur97@gmail.com>
2021-09-27 20:00:29 +0900

    add calculator and game

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,25 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+type Command = "add" | "substract" | "multiply" | "divide" | "remainder";
+
+function calculate(action: Command, num1: number, num2: number): number {
+  if (action === "add") {
+    return num1 + num2;
+  } else if (action === "substract") {
+    return num1 - num2;
+  } else if (action === "multiply") {
+    return num1 * num2;
+  } else if (action === "divide") {
+    return num1 / num2;
+  } else if (action === "remainder") {
+    return num1 % num2;
+  }
+  throw new Error("unknown error");
+}
+
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,31 @@
 /**
  * Let's make a game 🕹
  */
+type Direction = "up" | "down" | "left" | "right";
+
+function move(direction: Direction) {
+  switch (direction) {
+    case "up":
+      return position.y++;
+    case "down":
+      return position.y--;
+    case "left":
+      return position.x--;
+    case "right":
+      return position.x++;
+    default:
+      throw new Error(`undefined ${direction}`);
+  }
+}
+
+const position = { x: 0, y: 0 };
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
